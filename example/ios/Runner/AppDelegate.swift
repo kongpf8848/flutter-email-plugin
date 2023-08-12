1ac3a6b64cd8b7918243dc8eea9bbf8c74e708d8
Jack kong <kongpf8848@gmail.com>
2023-08-12 11:07:38 +0800

    update ios code

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -11,11 +11,10 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
               
+              Cryptor.cryptorInit()
               CZExchange.exchangeInit()
       
              
-              
-      
               let flutterViewController: FlutterViewController = window.rootViewController as! FlutterViewController
               
               self.ewsChannel = FlutterMethodChannel(name: "zenmen_flutter_email", binaryMessenger: flutterViewController.binaryMessenger)
@@ -32,17 +31,22 @@ import Flutter
                           let password=arguments["password"] as! String
                           let domain=arguments["domain"] as! String
                           let pointer=CZExchange.newSession(uri: url, email: address, password:password ,domain:domain)
-                          let session = self!.convertPointerToUInt64(pointer:pointer)
+                          let session = UInt(bitPattern: pointer!)
+                          print("++++++++++ews_new_session:",session)
                           result(session);
-                      case "ews_check_account":
+                     case "ews_check_account":
                           let arguments = call.arguments as! [String: Any]
-                          var session = arguments["session"] as! UInt64
-                          let response=CZExchange.checkAccount(sess: UnsafeMutableRawPointer(&session))
+                          var session = arguments["session"] as! UInt
+                          print("++++++++++ews_check_account,session:",session)
+                          let pointer=UnsafeMutableRawPointer(bitPattern: session)
+                          let response=CZExchange.checkAccount(sess: pointer!)
                           result(response)
                       case "ews_get_folders":
                           let arguments = call.arguments as! [String: Any]
-                          var session = arguments["session"] as! UInt64
-                          let response=CZExchange.getFolders(sess: UnsafeMutableRawPointer(&session))
+                          var session = arguments["session"] as! UInt
+                          print("++++++++++ews_get_folders,session:",session)
+                          let pointer=UnsafeMutableRawPointer(bitPattern: session)
+                          let response=CZExchange.getFolders(sess: pointer!)
                           result(response)
                       default:
                           result(FlutterMethodNotImplemented)
@@ -51,8 +55,4 @@ import Flutter
              return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
-  func convertPointerToUInt64(pointer: UnsafeMutableRawPointer?) -> UInt64? {
-    let convertedValue = pointer?.assumingMemoryBound(to: UInt64.self).pointee
-    return convertedValue
-  }
 }
